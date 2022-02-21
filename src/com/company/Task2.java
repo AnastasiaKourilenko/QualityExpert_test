1f7fa00f8a835af3f11032e2d40f59f75e20c613
Anastasia <anastasia@kourilenko.ru>
2022-02-21 20:40:25 +0300

    minor fixes

--- src/com/company/Task2.java
+++ src/com/company/Task2.java
@@ -7,22 +7,21 @@ import java.util.Map;
 
 public class Task2 {
 
-    private List<Integer> denominations = Arrays.asList(5000, 2000, 1000, 500, 200, 100, 50, 10, 5, 2, 1);
+    private final List<Integer> DENOMINATOINS = Arrays.asList(5000, 2000, 1000, 500, 200, 100, 50, 10, 5, 2, 1);
 
 
     public void change(int price, int amount) {
-        Map<Integer, Integer> changeDenomination = new HashMap<>();
+//        Map<Integer, Integer> changeDenomination = new HashMap<>();
         int change = amount - price;
         while (change != 0) {
-            for (Integer denomination : denominations) {
+            for (Integer denomination : DENOMINATOINS) {
                 if (change > denomination) {
                     int count = change / denomination;
                     System.out.println(denomination + "руб: " + count + "шт.");
-                    changeDenomination.put(denomination, count);
+//                    changeDenomination.put(denomination, count);
                     change = change % denomination;
                 }
             }
         }
-
     }
 }
